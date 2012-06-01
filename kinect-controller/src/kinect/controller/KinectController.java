87792c193f781ba2dacb30d0f7c3af393e961251
techjailer <admin@techjail.net>
2012-06-01 06:05:22 +0545

    just opens localhost

--- kinect-controller/src/kinect/controller/KinectController.java
+++ kinect-controller/src/kinect/controller/KinectController.java
@@ -8,8 +8,8 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 /**
@@ -27,20 +27,10 @@ public class KinectController extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        WebView wb = new WebView();
+        WebEngine we = wb.getEngine();
+        we.load("http://localhost/");
+        primaryStage.setScene(new Scene(wb, 300, 250));
         primaryStage.show();
     }
 }
