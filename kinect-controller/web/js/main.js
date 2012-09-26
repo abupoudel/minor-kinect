a4d7a2072d474dd33fd90b1730095f3b45efa3fa
abupoudel <admin@techjail.net>
2012-09-26 18:24:15 +0545

    File and folder browsing made possible

--- kinect-controller/web/js/main.js
+++ kinect-controller/web/js/main.js
@@ -8,16 +8,14 @@ $(document).ready(function(){
     $(document).mousemove(function(event){
         $(".cursor").css('left',event.pageX - 64);
         $(".cursor").css('top',event.pageY - 64);
-    //$(".position").html("(" + event.pageX + "," + event.pageY + ")");
+        $(".position").html("(" + event.pageX + "," + event.pageY + ")");
     });
 	
     $(".pageUp").live({
         click: function(){
             if(page == 0 || currentPage == 0){
-                $(this).html(currentPage);
             } else {
                 currentPage++;
-                $(this).html(currentPage);
                 $(".frame").animate({
                     top: (443*currentPage)
                 }, 500, function(){
@@ -41,10 +39,8 @@ $(document).ready(function(){
     $(".pageDown").live({
         click: function(){
             if(page == 1 || currentPage == -page+1){
-                $(this).html(currentPage);
             } else {
                 currentPage--;
-                $(this).html(page);
                 $(".frame").animate({
                     top: (443*currentPage)
                 }, 500, function(){
@@ -64,7 +60,22 @@ $(document).ready(function(){
             $("#cursor").fadeTo("fast", 0.3);
         }
     });
-	
+
+    $(".lists-sub").live({
+        mouseover: function(){
+            var topFolder = $(this);
+            $('#cursor').animate({
+                opacity: 1
+            }, 2000, function() {
+                topFolder.click();
+                $("#cursor").fadeTo("fast", 0.3);
+            });
+        },
+        mouseout: function(){
+            $("#cursor").stop();
+            $("#cursor").fadeTo("fast", 0.3);
+        }
+    });
 	
     $(".pane").mouseover(function(){
         $(this).stop();
