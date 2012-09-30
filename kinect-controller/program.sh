14e9f38672495fb10e5816bbf217f04e90262fea
abupoudel <admin@techjail.net>
2012-09-30 22:06:27 +0545

    complete package run

--- kinect-controller/program.sh
+++ kinect-controller/program.sh
@@ -1,1 +1,2 @@
-java -Djavafx.verbose=true -jar dist/kinect-controller.jar 
+python ../kinect.py
+/usr/lib/jvm/java-1.7.0-openjdk-amd64/jre/bin/java -Djavafx.verbose=true -jar dist/kinect-controller.jar 
