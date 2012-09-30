064c9938722b5f567ce9cad16913571a5fadb0e8
abupoudel <admin@techjail.net>
2012-09-30 21:41:09 +0545

    awesome openNI implemented hand detection

--- kinect.py
+++ kinect.py
@@ -1,111 +1,68 @@
-import freenect
-import cv
-import frame_convert
-import time
-import cython
-import Image
+#! /usr/bin/python
+import os
+from openni import *
 
-keep_running = True
-myAngle = 10
+global old_x
+global old_y
 
-print "Press ESC to exit | \"w\" to tilt sensor up | \"s\" to tilt sensor down"
+context = Context()
+context.init()
 
-def detect(image):
-    image_size = cv.GetSize(image)
- 
-    # create grayscale version
-    grayscale = cv.CreateImage(image_size, 8, 1)
-    cv.CvtColor(image, grayscale, cv.CV_BGR2GRAY)
- 
-    # create storage
-    storage = cv.CreateMemStorage(0)
-    #cv.ClearMemStorage(storage)
- 
-    # equalize histogram
-    cv.EqualizeHist(grayscale, grayscale)
- 
-    # detect objects
-    cascade = cv.Load('hand_cascade.xml')
-    faces = cv.HaarDetectObjects(grayscale, cascade, storage, 1.2, 2,cv.CV_HAAR_DO_CANNY_PRUNING, (150,150))
- 
-    if faces:
-        print 'hand detected!'
-        for (x,y,w,h),n in faces:
-			cv.Rectangle(image, (x,y), (x+w,y+h), 255)
-			print "(%d,%d)" % (x , y)
+depth_generator = DepthGenerator()
+depth_generator.create(context)
+depth_generator.set_resolution_preset(RES_VGA)
+depth_generator.fps = 30
 
-def display_depth(dev, data, timestamp):
-    global keep_running
-	
-    cv.Image = frame_convert.pretty_depth_cv(data)
-    cv.Flip(cv.Image,None,1)
-    image = cv.Image
-    #detect(image)
-    #cv.ShowImage('Depth' ,grayscale)
-	
-def display_rgb(dev, data, timestamp):
-	
-    cv.Image = frame_convert.video_cv(data)
-    cv.Flip(cv.Image,None,1)
-    image = cv.Image
-    size = cv.GetSize(image)
-    #hsv = cv.CreateImage(size, 8, 3)
-    #hue = cv.CreateImage(size, 8, 1)
-    #sat = cv.CreateImage(size, 8, 1)
-    #val = cv.CreateImage(size, 8, 1)
-    #hands = cv.CreateImage(size, 8, 1)
-    #cv.CvtColor(image, hsv, cv.CV_BGR2HSV)
+gesture_generator = GestureGenerator()
+gesture_generator.create(context)
+gesture_generator.add_gesture('Wave')
 
-    #cv.Split(hsv, hue, sat, val, None)    
-    #cv.SaveImage("out.jpg",image);
-    detect(image)
-    cv.ShowImage('Live', image)
-    #cv.ShowImage('Hue', hue)
-    #cv.ShowImage('Saturation', sat)
+hands_generator = HandsGenerator()
+hands_generator.create(context)
 
-    #cv.Threshold(hue, hue, 10, 255, cv.CV_THRESH_TOZERO) #set to 0 if <= 10, otherwise leave as is
-    #cv.Threshold(hue, hue, 244, 255, cv.CV_THRESH_TOZERO_INV) #set to 0 if > 244, otherwise leave as is
-    #cv.Threshold(hue, hue, 0, 255, cv.CV_THRESH_BINARY_INV) #set to 255 if = 0, otherwise 0
-    #cv.Threshold(sat, sat, 64, 255, cv.CV_THRESH_TOZERO) #set to 0 if <= 64, otherwise leave as is
-    #cv.EqualizeHist(sat, sat)
+# Declare the callbacks
+# gesture
+def gesture_detected(src, gesture, id, end_point):
+    print "Detected gesture:", gesture
+    hands_generator.start_tracking(end_point)
+# gesture_detected
 
-    #cv.Threshold(sat, sat, 64, 255, cv.CV_THRESH_BINARY) #set to 0 if <= 64, otherwise 255
+def gesture_progress(src, gesture, point, progress): pass
+# gesture_progress
 
-    #cv.ShowImage('Saturation threshold', sat)
-    #cv.ShowImage('Hue threshold', hue)
+def create(src, id, pos, time):
+    global old_x,old_y
+    print 'Create ', id, pos
+    old_x = int((640-pos[0])*2.134375);
+    old_y = int((480-pos[1])*1.6);
+# create
 
-    #cv.Mul(hue, sat, hands)
-    #detect(hands)
-    
+def update(src, id, pos, time):
+    global old_x, old_y
+    #print 'Update ', id, pos
+    x = int((320-pos[0])*2.134375);
+    y = int((480-pos[1])*1.6);
+    print x, y, (old_x-x)*(old_x-x) + (old_y-y)*(old_y-y)
+    if((old_x-x)*(old_x-x) + (old_y-y)*(old_y-y)) > 150 :
+        old_x = x
+        old_y = y
+    command = "xdotool mousemove {} {}".format(old_x, old_y)
+    os.system(command)
+# update
 
-    #smooth + threshold to filter noise
-    #cv.Smooth(hands, hands, smoothtype=cv.CV_GAUSSIAN, param1=13, param2=13)
-    #cv.Threshold(hands, hands, 200, 255, cv.CV_THRESH_BINARY)
-    #cv.ShowImage('Hands', hands)
-    
-    #cv.ShowImage('RGB' ,cv.Image)
+def destroy(src, id, time):
+    print 'Destroy ', id
+# destroy
 
-def body(dev,ctx):
-		global keep_running
-		global myAngle
-		k = (cv.WaitKey(10) & 255)
-		if k == 27:
-			print "Exiting..."
-			keep_running = False
-		elif chr(k) == 'w' or chr(k) == 'W':
-			myAngle = myAngle + 5
-		elif chr(k) == 's' or chr(k) == 'S':
-			myAngle = myAngle - 5
-		freenect.set_led(dev,1)
-		freenect.set_tilt_degs(dev,myAngle)
-		if not keep_running:
-			freenect.set_led(dev,0)
-			raise freenect.Kill
-   
-freenect.runloop(video = display_rgb, depth = display_depth, body=body)
+# Register the callbacks
+gesture_generator.register_gesture_cb(gesture_detected, gesture_progress)
+hands_generator.register_hand_cb(create, update, destroy)
 
+# Start generating
+context.start_generating_all()
 
-# Some tests on RGB
-#	dst = cv.CreateImage(cv.GetSize(cv.Image), cv.IPL_DEPTH_16S, 3)
-#	laplace = cv.Laplace(cv.Image, dst)
-#	cv.SaveImage("laplace.png" , dst)
+print 'Make a Wave to start tracking...'
+
+while True:
+    context.wait_any_update_all()
+# while
